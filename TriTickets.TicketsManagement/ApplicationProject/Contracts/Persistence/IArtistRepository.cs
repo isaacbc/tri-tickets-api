6abb6c90a7fb1f8ddffe3aa96da35807cdebf090
Isaac Bueno <isaacbc42@gmail.com>
2026-04-25 00:53:22 -0300

    add repositories

---
+++ TriTickets.TicketsManagement/ApplicationProject/Contracts/Persistence/IArtistRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationProject.Contracts.Persistence
+{
+    public interface IOrderItemRepository : IAsyncRepository<OrderItem>
+    {
+    }
+}
---
+++ TriTickets.TicketsManagement/ApplicationProject/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationProject.Contracts.Persistence
+{
+    public interface IAsyncRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(Guid Id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(string id);
+
+    }
+}
