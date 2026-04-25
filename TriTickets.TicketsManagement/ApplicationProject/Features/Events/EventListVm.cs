f533f2533ccf729190cc2a4c90188fbfc41ad5a0
Isaac Bueno <isaacbc42@gmail.com>
2026-04-25 00:54:03 -0300

    add get events list query handler

---
+++ TriTickets.TicketsManagement/ApplicationProject/Features/Events/EventListVm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationProject.Features.Events
+{
+    public class EventListVm
+    {
+        public Guid EventId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
