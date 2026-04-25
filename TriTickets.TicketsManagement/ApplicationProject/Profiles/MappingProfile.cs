cf698721b8b893fe838b241a6409a82f0314f71f
Isaac Bueno <isaacbc42@gmail.com>
2026-04-25 00:54:36 -0300

    add application service and mapping profile

---
+++ TriTickets.TicketsManagement/ApplicationProject/Profiles/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationProject.Features.Events;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationProject.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Event, EventListVm>().ReverseMap();
+            CreateMap<Event, EventDetailVm>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Artist, ArtistDto>().ReverseMap();
+
+
+        }
+
+    }
+}
